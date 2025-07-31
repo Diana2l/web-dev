3ca4ef18b0742f2ea99d2610eff2095f1ee0ffae
Diana2l <diana.cheptoo.j@gmail.com>
2025-07-31 08:12:59 +0300

    added to includes

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+    <p>&copy; <?php echo date("Y"); ?> ChevralTech. All rights reserved.</p>
+    <p>Contact us at <a href="mailto:chevraltech@gmail.com">chevraltech@gmail.com</a></p>
+</div>
+</body>
+</html>
